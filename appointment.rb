9d4ad69db38f16ae9c58079bd66293fb8d22e2fd
Alex Fakhri <alexfakhri@me.com>
2016-11-19 17:12:46 +0000

    Added if statment to script to ensure time is provided

--- appointment.rb
+++ appointment.rb
@@ -1,6 +1,9 @@
 require_relative './lib/availability.rb'
 
-time_requested = ARGV[0]
-
-appointment = Availability.new(time_requested)
-puts appointment.appoinment_time
+if ARGV.empty?
+  puts "Please provide an appointment time eg. 12:10"
+else
+  time_requested = ARGV[0]
+  appointment = Availability.new(time_requested)
+  puts appointment.appoinment_time
+end
